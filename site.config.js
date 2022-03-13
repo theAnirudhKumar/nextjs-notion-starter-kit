b144dfb17a5e7e90b4dedcf526530d7872e6b9ec
Anirudh Kumar <28919756+theAnirudhKumar@users.noreply.github.com>
2022-03-13 16:15:03 +0530

    New Changes
    Jai Mata Di

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '21c38a22edde44318db21d3ef1910220',
-  
+  rootNotionPageId: '28b417aa90cf400380362e563bf0485a',
+ 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
